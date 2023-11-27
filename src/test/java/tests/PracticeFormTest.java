32946d4e238aa2e2c6e81f381e9c03e04de607d2
BoldyrevDV <Boldblrev@gmail.com>
2023-11-28 00:25:50 +0300

    Delete all in page

--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import javax.xml.crypto.dom.DOMCryptoContext;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -16,6 +18,8 @@ public class PracticeFormTest {
         Configuration.browserSize = "1540x1178";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000;
+
 
     }
 
@@ -24,6 +28,8 @@ public class PracticeFormTest {
         open("/automation-practice-form");
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('#footer').remove()");
+        executeJavaScript("$('#app > header').remove()");
+        executeJavaScript("$('#app > div > div > div.pattern-backgound.playgound-header').remove()");
 
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         $("#firstName").setValue("Alex");
@@ -37,15 +43,15 @@ public class PracticeFormTest {
         $(".react-datepicker__day--017").click();
         $("#subjectsInput").setValue("Physics").pressEnter();
         $("#hobbiesWrapper").$(byText("Reading")).click();
+
         $("#uploadPicture").uploadFromClasspath("Img/Pic.png");
         $("#currentAddress").setValue("Some street 1");
         $("#state").click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
 
-//        $("#submit").;
-        executeJavaScript("$('#submit').click");
 
 //
 //        $("#output #name").shouldHave(text("Alex"));
